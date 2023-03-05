be294376d7eb21d01c5deecf126d4d4af9706175
pawankamdi <pawankamdi26@gmail.com>
2023-03-05 10:18:57 +0530

    Add project files.

---
+++ Using/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Using
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            using(Systemresource res = new Systemresource())
+            {
+                Console.WriteLine("A");
+            }
+            Console.WriteLine("B");
+            Console.ReadLine();
+        }
+    }
+}
+class Systemresource : IDisposable
+{
+    public void Dispose()
+    {
+        Console.WriteLine("C");
+    }
+}
+
+
